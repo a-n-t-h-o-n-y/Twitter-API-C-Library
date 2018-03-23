527b5c231c0bd84044251015bf9a82f01877ba6e
Anthony Leedom <anthonym.leedom@gmail.com>
2018-03-23 15:37:37 -0500

    Update CMakeLists.txt

--- src/place.cpp
+++ src/place.cpp
@@ -1,28 +1,27 @@
 #include "objects/place.hpp"
 
+#include <boost/property_tree/ptree.hpp>
 #include <sstream>
 #include <string>
-#include <boost/property_tree/ptree.hpp>
 
 namespace tal {
 
 Place_data::operator std::string() const {
     std::stringstream ss;
-    ss << "attributes:\n" << attributes
-       << "\nbounding_box:\n" << bounding_box
-       << "\ncountry: " << country
-       << "\ncoutry_code: " << country_code
-       << "\nfull_name: " << full_name
-       << "\nid: " << id
-       << "\nname: " << name
-       << "\nplace_type: " << place_type
+    ss << "attributes:\n"
+       << attributes << "\nbounding_box:\n"
+       << bounding_box << "\ncountry: " << country
+       << "\ncoutry_code: " << country_code << "\nfull_name: " << full_name
+       << "\nid: " << id << "\nname: " << name << "\nplace_type: " << place_type
        << "\nurl: " << url;
     return ss.str();
 }
 
 void Place_data::construct(const boost::property_tree::ptree& tree) {
-    attributes = Place_attributes{tree.get_child("attributes", boost::property_tree::ptree())};
-    bounding_box = Bounding_box{tree.get_child("bounding_box", boost::property_tree::ptree())};
+    attributes = Place_attributes{
+        tree.get_child("attributes", boost::property_tree::ptree())};
+    bounding_box = Bounding_box{
+        tree.get_child("bounding_box", boost::property_tree::ptree())};
     country = tree.get<std::string>("country", "");
     country_code = tree.get<std::string>("country_code", "");
     full_name = tree.get<std::string>("full_name", "");
